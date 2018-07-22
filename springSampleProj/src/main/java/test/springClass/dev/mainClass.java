39a67eeab82339cf1655fef75c1a572cabf5915f
marora224 <m.arora224@gmail.com>
2018-07-22 18:07:47 -0400

    test changes.

--- springSampleProj/src/main/java/test/springClass/dev/mainClass.java
+++ springSampleProj/src/main/java/test/springClass/dev/mainClass.java
@@ -3,7 +3,7 @@ package test.springClass.dev;
 public class mainClass {
 
 	public static void main (String [] args) {
-		System.out.println("running main class123");
+		System.out.println("running main class1244");
 	}
 	
 	
